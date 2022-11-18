be2ec270ecd249aa3bca6b66c3a8de8c417e1861
Samuli Massinen <samuli.massinen@ubigu.fi>
2022-11-18 15:46:19 +0200

    PDF parser ja PDF/A ver 1

---
+++ data/pdf/pdf-a-conv.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 17 15:44:37 2022
+
+@author: smassine
+"""
+
+import os
+import ghostscript
+
+print("\n PDF to PDF/A Conversion \n")
+
+# ghostscript package used
+# Output: PDF/A - 1b conversion
+
+filepath1 = input(" Enter the File path: ")
+
+Output1 = input(" Enter the Output path: ")
+
+output = Output1 + "\\"
+
+filepath = filepath1 + "\\"
+
+filelist = os.path.isdir(filepath) # specified path is an existing directory or not
+
+
+# os.listdir = list the file list in the directory
+for fname in os.listdir(filepath):
+    
+    print(fname)
+    if not fname.endswith(".pdf"):
+        continue
+    path = os.path.join(filepath, fname) 
+    print(path)
+    
+    #print(fname)
+    #print(path)
+    ghostScriptExec = ['gs', '-dPDFA', '-dBATCH', '-dNOPAUSE', '-dUseCIEColor', '-sProcessColorModel=DeviceRGB',
+                   '-sDEVICE=pdfwrite', '-dFastWebView', '-dAutoRotatePages=/None', '-sPDFACompatibilityPolicy=1',
+                   '-sOutputFile='+ output + 'PDFA-' + fname, path]
+    ghostscript.Ghostscript(*ghostScriptExec)
+    #print(filepath)
+    
+print("\n Conversion Completed\n")
